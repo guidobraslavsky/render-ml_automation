43557105dfccaffcd2fe098f6301bbf40ed8d7b9
Guido Braslavsky <braslavskyguido@gmail.com>
2026-03-06 19:12:55 -0300

    app.py port change

--- app.py
+++ app.py
@@ -38,4 +38,7 @@ app.register_blueprint(admin_bp)
 app.register_blueprint(complaint_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(host="0.0.0.0", port=port)
